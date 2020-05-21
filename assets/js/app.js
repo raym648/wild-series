2438038d7f4389f2ddbfad0f03b9b7f7d187beca
raym648 <raymond.francius@gmail.com>
2020-05-21 17:12:54 +0200

    modification quete 04 - Installation du module Webpach Encore

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,10 +5,15 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-import $ from 'jquery';
+// import from 'jquery';
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
